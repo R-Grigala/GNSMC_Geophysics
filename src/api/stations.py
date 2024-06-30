cff7afbcb2869eeb8bfd47d44bb5864febd2e5c8
R-Grigala <r.grigalashvili777@gmail.com>
2024-06-30 15:26:18 +0400

    Add Database and models

--- src/api/stations.py
+++ src/api/stations.py
@@ -1,8 +1,10 @@
 from flask_restx import Resource
 from flask import render_template
+from werkzeug.exceptions import NotFound
 
 from src.extensions import api
-from src.api.nsmodels import station_ns, station_parser
+from src.api.nsmodels import station_ns, station_model, station_parser
+from src.models import Stations
 
 data_list = [
     {
@@ -101,49 +103,67 @@ data_list = [
 @station_ns.doc(responses={200: 'OK', 400: 'Invalid Argument'})
 class ViewStationsAPI(Resource):
 
+    @station_ns.marshal_list_with(station_model)
     def get(self):
-        return data_list, 200
+        stations = Stations.query.all()
+
+        return stations, 200
     
 @station_ns.route('/view_station/<int:id>')
-@station_ns.doc(responses={200: 'OK', 400: 'Invalid Argument'})
+@station_ns.doc(responses={200: 'OK', 404: 'Station not found'})
 class ViewStationAPI(Resource):
+    @station_ns.marshal_with(station_model)
     def get(self, id):
-        return data_list[id], 200
+        station = Stations.query.get(id)
+        if not station:
+            raise NotFound("Station not found")
+        
+        return station, 200
     
 @station_ns.route('/create_station')
 @station_ns.doc(responses={200: 'OK', 400: 'Invalid Argument'})
 class CreateStationAPI(Resource):
 
-    @station_ns.doc(parser=station_parser)
+    @station_ns.expect(station_parser)
     def post(self):
-
         parser = station_parser.parse_args()
 
-        if parser["name"] == "name":
-            return "Successfully updated Station", 200
-        
-        return "Bad request", 400
+        new_station = Stations(
+            station_code=parser["station_code"],
+            station_lat=parser["station_lat"],
+            station_long=parser["station_long"]
+        )
+        new_station.create()
+
+        return {"message": "Successfully created Station"}, 200
 
 @station_ns.route('/edit_station/<int:id>')
 @station_ns.doc(responses={200: 'OK', 400: 'Invalid Argument'})
 class EditStationAPI(Resource):
 
-    @station_ns.doc(parser=station_parser)
+    @station_ns.expect(station_parser)
     def put(self, id):
-
         parser = station_parser.parse_args()
 
-        if parser["name"] == "name":
-            return "Successfully updated Station", 200
-        
-        return "Bad request", 400
+        station = Stations.query.get(id)
+        if station:
+            station.station_code = parser["station_code"]
+            station.station_lat = parser["station_lat"]
+            station.station_long = parser["station_long"]
+            station.save()  
+            return {"message": "Successfully updated Station"}, 200
+        else:
+            raise NotFound("Station not found")
     
 
 @station_ns.route('/delete_station/<int:id>')
 @station_ns.doc(responses={200: 'OK', 400: 'Invalid Argument'})
 class DeleteStationAPI(Resource):
+
     def delete(self, id):
-        if id == 1:
-            return "Successfully delete Station", 200
-        
-        return "Bad request", 400
+        station = Stations.query.get(id)
+        if station:
+            station.delete()
+            return {"message": "Successfully deleted Station"}, 200
+        else:
+            raise NotFound("Station not found")
