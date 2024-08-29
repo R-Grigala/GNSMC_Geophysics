7d75986184f1131999d9745ea11ef701de285f96
R-Grigala <r.grigalashvili777@gmail.com>
2024-08-30 00:05:04 +0400

    add  GeophysicLogggingAPI

--- src/api/__init__.py
+++ src/api/__init__.py
@@ -1,7 +1,7 @@
 from src.extensions import api
 from src.api.projects import ProjectsListAPI, ProjectAPI, ProjectImageListAPI, ProjectImageAPI
 from src.api.geological import GeologicalAPI
-from src.api.geophysical import GeophysicalListAPI
+from src.api.geophysical import GeophysicalListAPI, GeophysicalAPI
 from src.api.geophysic.geophysic_seismic import GeophysicSeismicListAPI, GeophysicSeismicAPI
-from src.api.geophysic.geophysic_logging import GeophysicLoggingListAPI
+from src.api.geophysic.geophysic_logging import GeophysicLoggingListAPI, GeophysicLoggingAPI
 from src.api.geophysic.geophysic_electrical import GeophysicElectricalAPI
