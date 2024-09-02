27807094a65c41c9c56b8df9d6fc36b663ed0cd8
R-Grigala <r.grigalashvili777@gmail.com>
2024-09-02 11:14:57 +0400

    Add Flask-JWT-Extended

--- src/api/nsmodels/authentication.py
+++ src/api/nsmodels/authentication.py
@@ -1,4 +1,4 @@
-from flask_restx import reqparse, fields
+from flask_restx import reqparse, inputs
 from src.extensions import api
 
 
@@ -7,7 +7,7 @@ registration_ns = api.namespace('Authentification', description='API endpoint fo
 registration_parser = reqparse.RequestParser()
 registration_parser.add_argument('name', type=str, required=True, help="Name example: Roma (1-20 characters)")
 registration_parser.add_argument('lastname', type=str, required=True, help="LastName example: Grigalashvili (1-20 characters)")
-registration_parser.add_argument('email', type=str, required=True, help="Email example: roma.grigalashvili@iliauni.edu.ge")
+registration_parser.add_argument('email', type=inputs.email(check=True), required=True, help="Email example: roma.grigalashvili@iliauni.edu.ge")
 registration_parser.add_argument('password', type=str, required=True, help="Password example: Grigalash1")
 registration_parser.add_argument('role_name', type=str, required=False, help="Name of the role example: guest")
 
